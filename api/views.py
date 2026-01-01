7f39f303e962c475f125bd6714d117d70e587e35
AliJalili <pandaj200021@gmail.com>
2026-01-01 11:02:07 +0330

    feat: add user registration API with JWT token response

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from api.serializers import UserSerializer
 
-# Create your views here.
+
+class UserRegisterView(CreateAPIView):
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user':serializer.data,
+            'refresh':str(refresh),
+            'access':str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
