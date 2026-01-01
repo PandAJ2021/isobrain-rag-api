0ee13586e7611f387dfb926fe7eb20a93372179d
AliJalili <pandaj200021@gmail.com>
2026-01-01 11:02:47 +0330

    feat: add authentication and registration API endpoints

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api import views
+
+
+app_name = 'api-v1'
+urlpatterns = (
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+)
